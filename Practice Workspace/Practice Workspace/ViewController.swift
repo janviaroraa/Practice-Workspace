aa0b8991b40a74064dad2fa604620160e0eded01
Janvi Arora <jjanvi.arora18@gmail.com>
2023-05-23 11:20:46 +0530

    basic set-up

---
+++ Practice Workspace/Practice Workspace/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice Workspace
+//
+//  Created by Powerplay on 23/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
