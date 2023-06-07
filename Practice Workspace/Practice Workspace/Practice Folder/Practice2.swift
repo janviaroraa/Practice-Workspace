790e15da4c2932aa691f556be7ed2a9fae63f067
Janvi Arora <jjanvi.arora18@gmail.com>
2023-06-07 16:39:59 +0530

    practice

---
+++ Practice Workspace/Practice Workspace/Practice Folder/Practice2.swift
@@ -0,0 +1,25 @@
+//
+//  Practice2.swift
+//  Practice Workspace
+//
+//  Created by Powerplay on 07/06/23.
+//
+
+import UIKit
+
+class Practice2: UIViewController {
+    
+    private lazy var tableView: UITableView = {
+        let tbl = UITableView()
+        tbl.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tbl
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+}
