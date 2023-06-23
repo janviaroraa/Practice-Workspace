dbdb32e2fa8ed4481d5069992f490a4f732fa3fb
Janvi Arora <jjanvi.arora18@gmail.com>
2023-06-23 16:00:38 +0530

    [pod installed

--- Practice Workspace/Pods/Target Support Files/Pods-Practice Workspace-Practice WorkspaceUITests/Pods-Practice Workspace-Practice WorkspaceUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Practice_Workspace_Practice_WorkspaceUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Practice_Workspace_Practice_WorkspaceUITests
-@end
